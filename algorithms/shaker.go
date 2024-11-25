5da8e3a9d9a3f49b178c9efe73f3a51e71dd59ac
dimitrijjedich <dimitrij@startup-werk.de>
2024-11-25 10:48:32 +0100

    add initial implementation

--- algorithms/shaker.go
+++ algorithms/shaker.go
@@ -1,5 +1,25 @@
 package algorithms
 
 func ShakerSort(arr []int) []int {
-	return arr
+	n := len(arr)
+	result := make([]int, n)
+	copy(result, arr)
+
+	sorted := false
+	for !sorted {
+		sorted = true
+		for i := 0; i < n-1; i++ {
+			if result[i] > result[i+1] {
+				result[i], result[i+1] = result[i+1], result[i]
+				sorted = false
+			}
+		}
+		for i := n - 1; i > 0; i-- {
+			if result[i] < result[i-1] {
+				result[i], result[i-1] = result[i-1], result[i]
+				sorted = false
+			}
+		}
+	}
+	return result
 }

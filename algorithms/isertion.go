76cc43ac06106caf17bf8f4af2b64a87def6429f
dimitrijjedich <dimitrij@startup-werk.de>
2024-11-25 07:37:37 +0100

    add initial implementation

--- algorithms/isertion.go
+++ algorithms/isertion.go
@@ -1,5 +1,19 @@
 package algorithms
 
 func InsertionSort(arr []int) []int {
-	return arr
+	n := len(arr)
+	result := make([]int, n)
+	copy(result, arr)
+
+	for i := 0; i < n; i++ {
+		for j, element := i-1, result[i]; j >= 0; j-- {
+			if element < result[j] {
+				result[j], result[j+1] = result[j+1], result[j]
+			} else {
+				result[j+1] = element
+				break
+			}
+		}
+	}
+	return result
 }

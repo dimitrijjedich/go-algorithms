9b5c6d8931d5c95132d035c293a464fc9deb94cb
dimitrijjedich <dimitrij@startup-werk.de>
2024-11-27 06:33:39 +0100

    add initial implementation

--- algorithms/quick.go
+++ algorithms/quick.go
@@ -5,5 +5,18 @@ func QuickSort(arr []int) []int {
 	if n == 0 {
 		return []int{}
 	}
-	return arr
+
+	left := make([]int, 0)
+	right := make([]int, 0)
+	pivot := arr[0]
+	for i := 1; i < n; i++ {
+		if arr[i] < pivot {
+			left = append(left, arr[i])
+		} else {
+			right = append(right, arr[i])
+		}
+	}
+	result := append(left, pivot)
+	result = append(result, right...)
+	return result
 }

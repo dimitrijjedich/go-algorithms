7d788ea244322d7e34bcde4c2fff2cf1849070f8
dimitrijjedich <dimitrij@startup-werk.de>
2024-11-29 08:29:06 +0100

    setup partitions

--- algorithms/radix.go
+++ algorithms/radix.go
@@ -5,4 +5,9 @@ func RadixSort(arr []int) []int {
 	n := len(arr)
 	result := make([]int, n)
 	copy(result, arr)
+
+	var partitions [10][]int
+	for i := range partitions {
+		partitions[i] = make([]int, 0)
+	}
 }

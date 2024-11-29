c9599f64aae1e590345d51a25ea89ca44453242d
dimitrijjedich <dimitrij@startup-werk.de>
2024-11-29 08:31:24 +0100

    add partitioning and collection logic for first iteration

--- algorithms/radix.go
+++ algorithms/radix.go
@@ -10,4 +10,18 @@ func RadixSort(arr []int) []int {
 	for i := range partitions {
 		partitions[i] = make([]int, 0)
 	}
+	divider := 10
+	for {
+		if len(partitions[0]) == n {
+			return partitions[0]
+		}
+		for i := 0; i < n; i++ {
+			partition := result[i] % divider
+			partitions[partition] = append(partitions[partition], result[i])
+		}
+		result = make([]int, 0)
+		for i := 0; i < 10; i++ {
+			result = append(result, partitions[i]...)
+		}
+	}
 }

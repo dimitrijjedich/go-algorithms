4e80d94e35482b702fe254941b96e78e6ce47c13
Dimitrij Jedich <140393501+dimitrijjedich@users.noreply.github.com>
2024-12-06 13:39:21 +0100

    Merge (#6)
    * initial commit
    
    * add terminating condition
    
    * add recursive call
    
    * add merging of arrays
    
    * fix typo
    
    * add tests for merge sort
    
    * fix return
    
    * fix splitting of array
    
    * add appending of last elements
    
    * fix appending of last elements
    
    * refactor appending of last elements

---
+++ algorithms/merge.go
@@ -0,0 +1,29 @@
+package algorithms
+
+func MergeSort(arr []int) []int {
+	n := len(arr)
+	if n <= 1 {
+		return arr
+	}
+	center := n / 2
+
+	left := MergeSort(arr[:center])
+	right := MergeSort(arr[center:])
+
+	nLeft := len(left)
+	nRight := len(right)
+	i, j := 0, 0
+	result := make([]int, 0)
+	for i < nLeft && j < nRight {
+		if left[i] < right[j] {
+			result = append(result, left[i])
+			i++
+		} else {
+			result = append(result, right[j])
+			j++
+		}
+	}
+	result = append(result, left[i:]...)
+	result = append(result, right[j:]...)
+	return result
+}

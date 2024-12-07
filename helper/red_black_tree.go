eff3ef2d18c40ab6daf829bf4cad4f6e8675ade7
dimitrijjedich <dimitrij@startup-werk.de>
2024-12-07 16:34:10 +0100

    add insert logic

--- helper/red_black_tree.go
+++ helper/red_black_tree.go
@@ -18,3 +18,26 @@ type RedBlackNode struct {
 type RedBlackTree struct {
 	root *RedBlackNode
 }
+
+func (tree *RedBlackTree) Insert(value int) {
+	newNode := &RedBlackNode{value: value, color: Red, parent: nil, left: nil, right: nil}
+	if tree.root == nil {
+		tree.root = newNode
+		return
+	}
+	pointer := tree.root
+	for {
+		if newNode.value < pointer.value && pointer.left != nil {
+			pointer = pointer.left
+		} else if newNode.value < pointer.value {
+			pointer.left = newNode
+			break
+		} else if pointer.right != nil {
+			pointer = pointer.right
+		} else {
+			pointer.right.left = newNode
+			break
+		}
+	}
+	newNode.parent = pointer
+}
